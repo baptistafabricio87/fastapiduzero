21e149f8b9323536bcde8ab5b8f60b182825a8e1
Fabricio Castro <baptistafabricio87@gmail.com>
2024-09-15 00:52:27 -0300

    add pythn version e primeiro teste

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,9 +6,9 @@ from fast_zero.app import app
 
 
 def test_read_root_retorna_ok_e_ola_mundo():
-    client = TestClient(app) # Arrange
-     
-    response = client.get('/') # Act
+    client = TestClient(app)  # Arrange
 
-    assert response.status_code == HTTPStatus.OK # Assert
-    assert response.json() == {'message': 'Olá Mundo!'} # Assert
+    response = client.get('/')  # Act
+
+    assert response.status_code == HTTPStatus.OK  # Assert
+    assert response.json() == {'message': 'Olá Mundo!'}  # Assert
