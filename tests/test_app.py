563a60375399aa4a27481b2fecf65f331822801e
Fabricio Castro <baptistafabricio87@gmail.com>
2024-09-15 11:25:23 -0300

    [exercicio] criar endpoint que retorna html e realizar teste

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,12 @@ def test_read_root_retorna_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK  # Assert
     assert response.json() == {'message': 'Olá Mundo!'}  # Assert
+
+
+def test_read_ola_html_retrona_ok():
+    client = TestClient(app)
+
+    response = client.get('/ola')
+
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1> Olá Mundo </h1>' in response.text
